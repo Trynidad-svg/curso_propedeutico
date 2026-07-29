469a914647ff473903ff9d1e2d9959a4fd8e63e9
Trynidad-svg <kevintrinidadg9@gmail.com>
2026-07-28 23:46:05 -0600

    Add files via upload

---
+++ hola_mundo/calcular_imc.py
@@ -0,0 +1,8 @@
+# Prgrama 3: Índice de masa corporal (IMC)
+
+peso = float(input("Peso (kg): "))
+altura = float(input("Altura (m): "))
+imc = peso / (altura ** 2)
+print("Su IMC es:" , imc)
+
+    
